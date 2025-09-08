44434cf73df21e47d07946e55c9a736768d473f6
EasyCanadianGamer <eyadi2005@gmail.com>
2025-09-07 21:28:34 -0400

    changed to bootloader 0.9 and used phils for guidance

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,65 @@
-// src/main.rs
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(emos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-fn main() {
-    // read env variables that were set in build script
-    let uefi_path = env!("UEFI_PATH");
-    let bios_path = env!("BIOS_PATH");
-    
-    // choose whether to start the UEFI or BIOS image
-    let uefi = true;
+extern crate alloc;
 
-    let mut cmd = std::process::Command::new("qemu-system-x86_64");
-    if uefi {
-        cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
-        cmd.arg("-drive").arg(format!("format=raw,file={uefi_path}"));
-    } else {
-        cmd.arg("-drive").arg(format!("format=raw,file={bios_path}"));
-    }
-    let mut child = cmd.spawn().unwrap();
-    child.wait().unwrap();
+use emos::println;
+use emos::task::{Task, executor::Executor, keyboard};
+use bootloader::{BootInfo, entry_point};
+use core::panic::PanicInfo;
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use emos::allocator;
+    use emos::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    println!("Welcome to EMOS!{}", "!");
+    emos::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    #[cfg(test)]
+    test_main();
+
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    emos::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    emos::test_panic_handler(info)
+}
+
+// async fn async_number() -> u32 {
+//     42
+// }
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
